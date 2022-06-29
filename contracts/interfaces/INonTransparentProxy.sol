57c5282ee4a01e348049d4d140d4f196f47d3be2
lucas-manuel <lucas@maple.finance>
2022-06-29 08:15:00 -0400

    feat: initial tests

---
+++ contracts/interfaces/INonTransparentProxy.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.8.7;
+
+interface INonTransparentProxy {
+
+    /**
+     *  @dev   Sets the implementation address.
+     *  @param newImplementation_ The address to set the implementation to.
+     */
+    function setImplementation(address newImplementation_) external;
+
+}
--- tests/SampleContract.t.sol
+++ tests/SampleContract.t.sol
@@ -1,22 +1,80 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.7;
 
+import { NonTransparentProxied } from "../contracts/NonTransparentProxied.sol";
+import { NonTransparentProxy }   from "../contracts/NonTransparentProxy.sol";
+
 import { TestUtils } from "../modules/contract-test-utils/contracts/test.sol";
 
-contract ERC20BaseTest is TestUtils { 
+contract NTPTestBase is TestUtils {
 
-    function setUp() public virtual { }
+    address constant ADMIN     = address(1);
+    address constant NOT_ADMIN = address(2);
 
-    function test_success() external {
-        assertTrue(true);
+    bytes32 constant ADMIN_SLOT          = bytes32(uint256(keccak256("eip1967.proxy.admin"))          - 1);
+    bytes32 constant IMPLEMENTATION_SLOT = bytes32(uint256(keccak256("eip1967.proxy.implementation")) - 1);
+
+    NonTransparentProxied implementation;
+
+    function setUp() public virtual {
+        implementation = new NonTransparentProxied();
     }
 
-    function test_failure() external {
-        assertTrue(false);
+}
+
+contract NTPConstructorTests is NTPTestBase {
+
+    function test_constructor() external {
+        NonTransparentProxy proxy = new NonTransparentProxy(ADMIN, address(implementation));
+
+        address adminStorage          = address(uint160(uint256(vm.load(address(proxy), ADMIN_SLOT))));
+        address implementationStorage = address(uint160(uint256(vm.load(address(proxy), IMPLEMENTATION_SLOT))));
+
+        assertEq(adminStorage,          ADMIN);
+        assertEq(implementationStorage, address(implementation));
+
+        // TODO: View functions on implementation
     }
 
-    function test_fuzz(uint256 x) external {
-        assertEq(x, x);
+}
+
+contract NTPSetImplementationFailureTests is NTPTestBase {
+
+    NonTransparentProxy proxy;
+
+    function setUp() public override virtual {
+        super.setUp();
+        proxy = new NonTransparentProxy(ADMIN, address(implementation));
+    }
+
+    function test_setImplementation_notAdmin() external {
+        vm.startPrank(NOT_ADMIN);
+        vm.expectRevert("NTP:SI:NOT_ADMIN");
+        proxy.setImplementation(address(10));
+    }
+
+}
+
+contract NTPSetImplementatinSuccessTests is NTPTestBase {
+
+    NonTransparentProxy proxy;
+
+    function setUp() public override virtual {
+        super.setUp();
+        proxy = new NonTransparentProxy(ADMIN, address(implementation));
+    }
+
+    function test_setImplementation_notAdmin() external {
+        address implementationStorage = address(uint160(uint256(vm.load(address(proxy), IMPLEMENTATION_SLOT))));
+
+        assertEq(implementationStorage, address(implementation));
+
+        vm.startPrank(ADMIN);
+        proxy.setImplementation(address(1));
+
+        implementationStorage = address(uint160(uint256(vm.load(address(proxy), IMPLEMENTATION_SLOT))));
+
+        assertEq(implementationStorage, address(1));
     }
 
 }
