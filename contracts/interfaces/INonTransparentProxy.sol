080fa6d5aa09cda87aae9f05f1084b3e4c2458cc
Preethi Senthil <preethisenthil@Preethis-MacBook-Pro.local>
2023-08-28 11:15:00 -0700

    feat: Add ImplementationSet event to setImplementation function (SC-13536)

--- contracts/interfaces/INonTransparentProxy.sol
+++ contracts/interfaces/INonTransparentProxy.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.7;
 interface INonTransparentProxy {
 
     /**
+     *  @dev   A new implementation address was set.
+     *  @param oldImplementationAddress_ The address of the old implementation.
+     *  @param newImplementationAddress_ The address of the new implementation.
+     */
+    event ImplementationSet(address indexed oldImplementationAddress_, address indexed newImplementationAddress_);
+
+    /**
      *  @dev   Sets the implementation address.
      *  @param newImplementation_ The address to set the implementation to.
      */
